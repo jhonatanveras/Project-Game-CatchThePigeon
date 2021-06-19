ad33294d04958f23538443638321fe5590817d0c
Jhonatan Veras <jveras@leroymerlin.com.br>
2021-06-19 16:19:28 -0300

    incluido função randomPosition

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 let height = 0;
 let width = 0;
 
-function resizingByCanvas () {
+function resizingByCanvas() {
     height = window.innerHeight;
     width = window.innerWidth;
-    
-    console.log( width, height);
+
+    //console.log( width, height);
 }
 
 resizingByCanvas();
 
-const posX = Math.floor(Math.random() * width);
-const posY = Math.floor(Math.random() * height);
+function randomPosition() {
+    let posX = Math.floor(Math.random() * width);
+    let posY = Math.floor(Math.random() * height);
 
-console.log (posX, posY);
+    //console.log (posX, posY);
+
+    const pigeon = document.createElement('img');
+    pigeon.src = './Img/pombo.png';
+    pigeon.className = 'mainPigeon';
+    pigeon.style.left = posX + 'px';
+    pigeon.style.top = posY + 'px';
+    pigeon.style.position = ("absolute")
+
+
+    document.body.appendChild(pigeon);
+
+}
