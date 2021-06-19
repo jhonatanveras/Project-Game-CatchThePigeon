1bb8e359c6ebecab00f238b80116a72fe2f33567
Jhonatan Veras <jveras@leroymerlin.com.br>
2021-06-19 14:21:34 -0300

    included screen size function

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const height = 0;
+const width = 0;
+
+function resizingByCanvas () {
+    const height = window.innerHeight;
+    const width = window.innerWidth;
+    
+    console.log( height, width);
+}
+
+resizingByCanvas();
